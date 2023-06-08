3df8d48988d4663aa38f282904fba5ed3cb369f5
PopSlime <popslime@qq.com>
2023-06-08 20:07:05 +0800

    Pull down SatelliteCompassView.

--- Controls/CompassView.cs
+++ Controls/CompassView.cs
@@ -6,51 +6,26 @@ using System;
 using System.Collections.Generic;
 
 namespace SensorLab.Controls {
-	public class CompassView : View {
+	public abstract class CompassView : View {
 		static readonly List<CompassView> _instances = new List<CompassView>();
 		public static void Redraw() {
 			foreach (var i in _instances) i.Invalidate();
 		}
 
-		static readonly Paint _strokeBg;
-		static readonly Paint _fillText;
-		static readonly Paint _fillId;
-		static readonly Paint _fillSatellite;
-		static readonly Color _colorFix;
-		static readonly Color _colorEphemeris;
-		static readonly Color _colorAlmanac;
-		static readonly Color _colorUnknown;
+		protected static readonly Paint _strokeBg;
+		protected static readonly Paint _fillText;
 
 		static CompassView() {
 			_strokeBg = new Paint { Color = Color.LightBlue, StrokeWidth = 0.02f, AntiAlias = true };
 			_strokeBg.SetStyle(Paint.Style.Stroke);
 			_fillText = new Paint { Color = Color.LightBlue, TextSize = 24f, TextAlign = Paint.Align.Center, AntiAlias = true };
 			_fillText.SetStyle(Paint.Style.Fill);
-			_fillId = new Paint { Color = Color.Green, TextSize = 12f, TextAlign = Paint.Align.Left, AntiAlias = true };
-			_fillId.SetStyle(Paint.Style.Fill);
-			_fillSatellite = new Paint { Color = Color.Green, AntiAlias = true };
-			_fillSatellite.SetStyle(Paint.Style.Fill);
-			_colorFix = Color.Green;
-			_colorEphemeris = Color.Yellow;
-			_colorAlmanac = Color.Red;
-			_colorUnknown = Color.Gray;
 		}
 
-		public CompassView(Context context) : base(context) {
-			_instances.Add(this);
-		}
-
-		public CompassView(Context context, IAttributeSet attrs) : base(context, attrs) {
-			_instances.Add(this);
-		}
-
-		public CompassView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) {
-			_instances.Add(this);
-		}
-
-		public CompassView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes) {
-			_instances.Add(this);
-		}
+		public CompassView(Context context) : base(context) { _instances.Add(this); }
+		public CompassView(Context context, IAttributeSet attrs) : base(context, attrs) { _instances.Add(this); }
+		public CompassView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) { _instances.Add(this); }
+		public CompassView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes) { _instances.Add(this); }
 
 		static float m_compassRotation;
 		public static float CompassRotation {
@@ -64,7 +39,7 @@ namespace SensorLab.Controls {
 		internal static Dictionary<SatelliteIdentifier, SatelliteStatus> Satellites;
 		internal static bool FlipRotation;
 
-		protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		protected sealed override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 			base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
 			var w = MeasureSpec.GetSize(widthMeasureSpec);
 			var h = MeasureSpec.GetSize(heightMeasureSpec);
@@ -72,8 +47,7 @@ namespace SensorLab.Controls {
 			SetMeasuredDimension(s, s);
 		}
 
-		const float _compassSize = 0.84f;
-		const float _satelliteSize = 0.02f;
+		protected const float _compassSize = 0.84f;
 		static readonly int[] _directions = new[] {
 			Resource.String.dir_n, Resource.String.dir_ne,
 			Resource.String.dir_e, Resource.String.dir_se,
@@ -81,7 +55,7 @@ namespace SensorLab.Controls {
 			Resource.String.dir_w, Resource.String.dir_nw,
 		};
 
-		protected override void OnDraw(Canvas canvas) {
+		protected sealed override void OnDraw(Canvas canvas) {
 			base.OnDraw(canvas);
 			float scale;
 			if (canvas.Width > canvas.Height) scale = canvas.Height / 2f;
@@ -91,49 +65,31 @@ namespace SensorLab.Controls {
 			_fillText.TextLocale = Java.Util.Locale.Default;
 			float fcoef = FlipRotation ? -1 : 1;
 
-			for (float i = 30; i <= 90; i += 30) {
-				canvas.DrawCircle(0, 0, i / 90 * _compassSize, _strokeBg);
-				canvas.Save();
-				canvas.Scale(0.004f, 0.004f);
-				canvas.DrawText((90 - i).ToString(), 0, (i / 90 * _compassSize - 0.02f) / 0.004f, _fillText);
-				canvas.Restore();
-			}
-
 			canvas.Rotate(-fcoef * CompassRotation, 0, 0);
 			for (int i = 0; i < _directions.Length; i++) {
 				canvas.DrawLine(0, -_compassSize, 0, -(_compassSize - 0.04f), _strokeBg);
-				canvas.Save();
-				canvas.Scale(0.004f, 0.004f);
-				canvas.DrawText(Resources.GetString(_directions[i]), 0, -(_compassSize + 0.04f) / 0.004f, _fillText);
-				canvas.Restore();
+				DrawText(canvas, Resources.GetString(_directions[i]), 0, -(_compassSize + 0.04f), _fillText);
 				canvas.Rotate(fcoef / _directions.Length * 360, 0, 0);
 			}
 			canvas.Rotate(fcoef * CompassRotation, 0, 0);
 
-			if (Satellites != null) {
-				lock (Satellites) {
-					foreach (var sat in Satellites) {
-						var mag = _compassSize * (90 - sat.Value.Elevation) / 90;
-						var ori = fcoef * (sat.Value.Azimuth - CompassRotation) / 180 * MathF.PI;
-						Color color;
-						if (sat.Value.IsUsedInFix) color = _colorFix;
-						else if (sat.Value.HasEphemerisData) color = _colorEphemeris;
-						else if (sat.Value.HasAlmanacData) color = _colorAlmanac;
-						else color = _colorUnknown;
-						_fillSatellite.Color = _fillId.Color = color;
-						float x = mag * MathF.Sin(ori), y = -mag * MathF.Cos(ori);
-						float satScale = MathF.Sqrt(sat.Value.CNR / 10);
-						canvas.DrawCircle(x, y, _satelliteSize * satScale, _fillSatellite);
-						canvas.Save();
-						canvas.Scale(0.004f, 0.004f);
-						canvas.DrawText(
-							string.Format("{0}-{1}", sat.Key.Constellation, sat.Key.SVID),
-							(x + 0.02f * satScale) / 0.004f, (y - 0.01f * satScale) / 0.004f, _fillId
-						);
-						canvas.Restore();
-					}
-				}
-			}
+			DrawContent(canvas, fcoef);
+		}
+
+		protected abstract void DrawContent(Canvas canvas, float flip);
+
+		protected void DrawMagnitude(Canvas canvas, float magnitude, string caption = null) {
+			canvas.DrawCircle(0, 0, magnitude * _compassSize, _strokeBg);
+			if (string.IsNullOrEmpty(caption)) return;
+			DrawText(canvas, caption, 0, magnitude * _compassSize - 0.02f, _fillText);
+		}
+
+		const float _textScale = 0.004f;
+		protected void DrawText(Canvas canvas, string caption, float x, float y, Paint fill) {
+			canvas.Save();
+			canvas.Scale(_textScale, _textScale);
+			canvas.DrawText(caption, x / _textScale, y / _textScale, fill);
+			canvas.Restore();
 		}
 	}
 }
---
+++ Controls/SatelliteCompassView.cs
@@ -0,0 +1,57 @@
+using Android.Content;
+using Android.Graphics;
+using Android.Util;
+using System;
+
+namespace SensorLab.Controls {
+	public class SatelliteCompassView : CompassView {
+		static readonly Paint _fillId;
+		static readonly Paint _fillSatellite;
+		static readonly Color _colorFix;
+		static readonly Color _colorEphemeris;
+		static readonly Color _colorAlmanac;
+		static readonly Color _colorUnknown;
+
+		static SatelliteCompassView() {
+			_fillId = new Paint { Color = Color.Green, TextSize = 12f, TextAlign = Paint.Align.Left, AntiAlias = true };
+			_fillId.SetStyle(Paint.Style.Fill);
+			_fillSatellite = new Paint { Color = Color.Green, AntiAlias = true };
+			_fillSatellite.SetStyle(Paint.Style.Fill);
+			_colorFix = Color.Green;
+			_colorEphemeris = Color.Yellow;
+			_colorAlmanac = Color.Red;
+			_colorUnknown = Color.Gray;
+		}
+
+		public SatelliteCompassView(Context context) : base(context) { }
+		public SatelliteCompassView(Context context, IAttributeSet attrs) : base(context, attrs) { }
+		public SatelliteCompassView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr) { }
+		public SatelliteCompassView(Context context, IAttributeSet attrs, int defStyleAttr, int defStyleRes) : base(context, attrs, defStyleAttr, defStyleRes) { }
+
+		const float _satelliteSize = 0.02f;
+		protected override void DrawContent(Canvas canvas, float flip) {
+			for (float i = 30; i <= 90; i += 30) {
+				DrawMagnitude(canvas, i / 90, (90 - i).ToString());
+			}
+
+			if (Satellites != null) {
+				lock (Satellites) {
+					foreach (var sat in Satellites) {
+						var mag = _compassSize * (90 - sat.Value.Elevation) / 90;
+						var ori = flip * (sat.Value.Azimuth - CompassRotation) / 180 * MathF.PI;
+						Color color;
+						if (sat.Value.IsUsedInFix) color = _colorFix;
+						else if (sat.Value.HasEphemerisData) color = _colorEphemeris;
+						else if (sat.Value.HasAlmanacData) color = _colorAlmanac;
+						else color = _colorUnknown;
+						_fillSatellite.Color = _fillId.Color = color;
+						float x = mag * MathF.Sin(ori), y = -mag * MathF.Cos(ori);
+						float satScale = MathF.Sqrt(sat.Value.CNR / 10);
+						canvas.DrawCircle(x, y, _satelliteSize * satScale, _fillSatellite);
+						DrawText(canvas, string.Format("{0}-{1}", sat.Key.Constellation, sat.Key.SVID), x + 0.02f * satScale, y - 0.01f * satScale, _fillId);
+					}
+				}
+			}
+		}
+	}
+}
