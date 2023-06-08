74f35e7a4175f4d6956fc978d40c76d1f0da2d9d
PopSlime <popslime@qq.com>
2023-06-08 12:04:29 +0800

    Fix app icon.

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -11128,7 +11128,7 @@ namespace SensorLab
 			public const int ic_launcher = 2131558400;
 			
 			// aapt resource value: 0x7F0D0001
-			public const int ic_launcher_round = 2131558401;
+			public const int ic_launcher_foreground = 2131558401;
 			
 			static Mipmap()
 			{
